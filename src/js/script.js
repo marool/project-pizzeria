9b39d8238a317120ff2218051aa28d5117f2b4d1
Marcin Kaczmarek <mkmarol@gmail.com>
2020-07-23 12:19:56 +0200

    add getElements and edit initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -59,6 +59,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
@@ -83,16 +84,26 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
 
-      const trigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
 
       /* START: click event listener to trigger */
 
-      trigger.addEventListener('click', function(event){
+      thisProduct.accordionTrigger.addEventListener('click', function(event){
         console.log('clicked');
 
         /* prevent default action for event */
@@ -128,6 +139,7 @@
       });
     }
   }
+
 
   const app = {
     initMenu: function () {
