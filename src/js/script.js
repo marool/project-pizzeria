810e725ecec2ffb3aa0cf27ebc9ab91830801191
Marcin Kaczmarek <mkmarol@gmail.com>
2020-07-29 18:58:31 +0200

    add class cart

--- src/js/script.js
+++ src/js/script.js
@@ -349,6 +349,26 @@
     }
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -378,8 +398,6 @@
       thisApp.initMenu();
     },
   };
-
-
 
   app.init();
 }
