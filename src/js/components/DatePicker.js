54bd01a9ab90af1a9e007b19a066cf4759721618
Marcin Kaczmarek <mkmarol@gmail.com>
2020-08-09 19:33:16 +0200

    add datepicked and hourpicker

---
+++ src/js/components/DatePicker.js
@@ -0,0 +1,53 @@
+import BaseWidget from './BaseWidget.js';
+import {utils} from '../utils.js';
+import {select, settings} from '../settings.js';
+
+
+class DatePicker extends BaseWidget {
+  constructor(wrapper){
+    super(wrapper, utils.dateToStr(new Date()));
+
+    const thisWidget = this;
+
+    thisWidget.dom = {};
+    thisWidget.dom.wrapper = wrapper;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+    thisWidget.initPlugin();
+  }
+
+  initPlugin() {
+    const thisWidget = this;
+
+    thisWidget.minDate = new Date(thisWidget.value);
+    thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+    /* global flatpickr */
+    flatpickr(thisWidget.dom.input, {
+      defaultDate: thisWidget.minDate,
+      minDate: thisWidget.minDate,
+      maxDate: thisWidget.maxDate,
+      'locale': {
+        'firstDayofWeek': 1
+      },
+      'disable': [
+        function(date) {
+          return (date.getDay() === 1);
+        }
+      ],
+      onChange: function(selectedDates, dateStr) {
+        thisWidget.value = dateStr;
+      }
+
+    });
+
+  }
+
+  parseValue(newValue) {
+    return newValue;
+  }
+
+  isValid() {
+    return true;
+  }
+
+}
+export default DatePicker;
